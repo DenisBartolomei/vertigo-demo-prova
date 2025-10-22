1500acd15edffa8c85c1ab50625e4c52c6a31844
denis bartolomei <denis.bartolomei@gmail.com>
2025-10-22 21:02:40 +0200

    corretta porta backend

--- backend/app.py
+++ backend/app.py
@@ -1682,6 +1682,7 @@ async def shutdown_event():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("backend.app:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run("backend.app:app", host="0.0.0.0", port=port, reload=False)
 
 
