c7b85ce5fc44acc2612262c470d23d2b8bc50d9f
denis bartolomei <denis.bartolomei@gmail.com>
2025-10-22 21:22:25 +0200

    risolto bug deploy

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, UploadFile, File, Form, HTTPException, Depends, Hea
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import StreamingResponse, Response
 from pydantic import BaseModel
+from typing import List
 import os
 import uuid
 import fitz  # PyMuPDF
